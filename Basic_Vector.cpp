e7c833e684b0e88a1a5ba7d2385b7a1d0ded2e9c
Aman Chouhan <ak6927887@gmail.com>
2025-09-30 22:16:44 +0530

    Create Basic_Vector.cpp

---
+++ Basic_Vector.cpp
@@ -0,0 +1,34 @@
+//vector<datatype>variable name;
+//example- vector<int>v;
+
+//vector<datatype>variable name(size of vector);
+//vector<int>v(4);
+
+//vector<datatype>variable name(size of vector, intialize with any value);
+//vector<int>v(4,2);
+//o/p- 2 2 2 2
+
+/*
+value insert
+vector<int>a;
+a.push_back(1);
+o/p- 1
+a.push_back(2);
+o/p- 1 2
+a.push_back(3);
+o/p- 1 2 3
+a.push_back(4);
+o/p- 1 2 3 4
+*/
+
+//vector<int>a={1,2,3,4,5,6};
+
+/*
+int n;
+cin>>n;
+vector<int>a(n);
+for(int i=0,i<n;i++){
+  cin>>a[i];
+*/
+
+
