d655f136adb04914eab1fbac7c771a06fd0f75dd
Aman Chouhan <ak6927887@gmail.com>
2025-09-30 22:32:28 +0530

    Update Basic_Vector.cpp

--- Basic_Vector.cpp
+++ Basic_Vector.cpp
@@ -31,4 +31,35 @@ for(int i=0,i<n;i++){
   cin>>a[i];
 */
 
+/*
+remove value
+vector<int>a={1,2,3,4,5,6};
+a.pop_back();
+//   0 1 2 3 4  indexing
+o/p- 1 2 3 4 5 
+a.erace(v.begain()+2);
+o/p- 1 2 _ 4 5
+     1 2 4 5
+a.clear();
+o/p-        all clear
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
